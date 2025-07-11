8b9ff83615329ca94cf84231172f6f197187a2e5
jal <jal@hayloft-it.ch>
2025-07-11 16:51:57 +0200

    sidenav fix

--- assets/js/sideNav.js
+++ assets/js/sideNav.js
@@ -2,6 +2,12 @@ $(document).ready(function() {
     const $hamburgerButton = $("#hamburger-button");
     const $closeButton = $("#close-button");
     const $sideNav = $("#side-nav");
+    const $buttonOpen = $(".button-open");
+    const $buttonClose = $(".button-close");
+    const $sideDropdown = $(".side-dropdown");
+
+    $sideDropdown.hide();
+    $buttonClose.hide();
 
     $hamburgerButton.on("click", function() {
         $sideNav.addClass("open");
@@ -9,5 +15,17 @@ $(document).ready(function() {
 
     $closeButton.on("click", function() {
         $sideNav.removeClass("open");
+    });
+
+    $buttonOpen.on("click", function() {
+        $buttonClose.show();
+        $buttonOpen.hide();
+        $sideDropdown.show("hidden");
+    });
+
+    $buttonClose.on("click", function() {
+        $buttonClose.hide();
+        $buttonOpen.show();
+        $sideDropdown.hide("hidden");
     });
 });
